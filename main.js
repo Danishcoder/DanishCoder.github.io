044c645f31638d521bce6cdbde193006588e0971
DanishCoder <Danishim66@gmail.com>
2022-07-15 12:44:09 -0400

    1.1

---
+++ main.js
@@ -0,0 +1,8 @@
+let linkButton = document.querySelector('#Linked-Button')
+
+
+function linkedButton (event) {
+    window.location.href = "https://www.linkedin.com/in/danish-mansoori-9389a5220/"
+}
+
+linkButton.addEventListener('click',linkedButton)
